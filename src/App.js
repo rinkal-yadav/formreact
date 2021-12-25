8d42e4c9d31c1d07caa51b1920a1f88a0c632761
rinkal-yadav <rinkal95@gmail.com>
2021-12-25 15:55:19 +0400

    update react form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Employee from './EmployeeDetails/Employee';
-import Employe from './EmployeeDetails/Employe';
 import './EmployeeDetails/style.css'
 
 function App() {
   return (
     <div className="App">
       <Employee/>
-      {/* <Employe/> */}
       
     </div>
   );
